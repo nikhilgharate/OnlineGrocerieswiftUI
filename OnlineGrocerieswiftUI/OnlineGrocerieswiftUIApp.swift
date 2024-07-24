a22e14b47cbe2199c9371326bb825196b1803d56
nikhilgharate <nikhilgharate365@gmail.com>
2024-07-24 13:30:14 +0530

    Initial Commit

---
+++ OnlineGrocerieswiftUI/OnlineGrocerieswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGrocerieswiftUIApp.swift
+//  OnlineGrocerieswiftUI
+//
+//  Created by iAURO on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGrocerieswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
